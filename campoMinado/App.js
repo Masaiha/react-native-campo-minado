b1146a18a8406d70393efb9a0d7b129a8567f729
Masaiha <masanori.iha@gmail.com>
2020-08-24 15:21:54 -0300

    criando board

--- campoMinado/App.js
+++ campoMinado/App.js
@@ -25,39 +25,54 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import Field from './src/components/Field';
 import Flag from './src/components/Flag';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
 import params from './src/params';
 
 export default class App extends Component {
+
+  constructor(...props) {
+    super(...props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    
+    return{
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
   render(){
     return(
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.welcome}>Iniciando O Jogo</Text>
-      <Text style={styles.welcome} >Tamanho da grade: {params.getRowsAmount() +"x"+ params.getColumnsAmount()}</Text>
-      <Field />
-      <Field opened/>
-      <Field opened nearMines={1}/>
-      <Field opened nearMines={2}/>
-      <Field opened nearMines={3}/>
-      <Field opened nearMines={6}/>
-      <Field mined/>
-      <Field mined opened/>
-      <Field mined opened exploded/>
-      <Field flagged />
-    </SafeAreaView>
-    );
-  }
+    <View style={styles.container}>
+      <Text>Iniciando O Jogo</Text>
+      <Text>Tamanho da grade: {
+            params.getRowsAmount() +"x"+ params.getColumnsAmount()}</Text>
+      <View style={styles.board}>
+        <MineField board={this.state.board} />
+      </View>
+    </View>
+    )};
 };
 
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'flex-end',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF'
   },
-  welcome:{
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },  
+  board: {
+    // alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
 });
